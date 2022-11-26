45ee5fc617be29c4ab28d6bd245789d0d15bb65e
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-11-26 08:01:02 +0530

    Adding test file

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+// import { render, screen } from '@testing-library/react';
+
+import React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+test("ToDo", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+
+  // after rendering our component
+  // use DOM APIs (query selector) to make assertions
+  expect(root.querySelector("h1").textContent).toBe("TODO");
+  // expect(root.querySelector("label").textContent).toBe("Add TODO");
+});
