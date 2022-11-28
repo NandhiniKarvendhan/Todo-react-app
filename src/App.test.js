29fe4a808ccab4f2ac355f6190747b3941e471df
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-11-28 11:49:10 +0530

    Updating test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,23 @@
 // import { render, screen } from '@testing-library/react';
 
+import { render } from "@testing-library/react";
 import React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
+// function render(component) {
+//   const root = document.createElement("div");
+//   ReactDOM.render(component, root);
+//   return getQueriesForElement(root);
+// }
+
 test("ToDo", () => {
-  const root = document.createElement("div");
-  ReactDOM.render(<App />, root);
+  const { getByText, getByLabelText } = render(<App />);
 
   // after rendering our component
   // use DOM APIs (query selector) to make assertions
-  expect(root.querySelector("h1").textContent).toBe("TODO");
+
   // expect(root.querySelector("label").textContent).toBe("Add TODO");
+  expect(getByText("TODO")).not.toBeNull();
+  // getByLabelText("Add TODO");
 });
