b12f7ffd0e566de464a0caf720e0c4781dd1be98
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-12-05 11:26:23 +0530

    updating simple TODO App

--- src/App.js
+++ src/App.js
@@ -1,77 +1,42 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function Todo({ todo, index, remove }) {
-  function handle() {
-    remove(index);
-  }
-  return (
-    <div className="todo" onClick={handle}>
-      {todo.text} (-)
-    </div>
-  );
-}
+function App() {
+  const [items, setItems] = React.useState([]);
+  const [value, setValue] = React.useState([]);
 
-function TodoForm({ addTodo }) {
-  const [value, setValue] = React.useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!value) return;
-    addTodo(value);
+    setItems([...items, value]);
     setValue("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        placeholder="Add a TODO"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </form>
+    <div>
+      <h1>TODO</h1>
+      <TodoList items={items} />
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="new-todo">Add todo:</label>
+        <input
+          id="new-todo"
+          placeholder="Add a TODO"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button>Add #{items.length + 1}</button>
+      </form>
+    </div>
   );
 }
 
-function App() {
-  const [todos, setTodos] = React.useState([
-    {
-      text: "learn react",
-      isCompleted: false,
-    },
-    {
-      text: "meet friend for lunch",
-      isCompleted: false,
-    },
-    {
-      text: "build todo app",
-      isCompleted: false,
-    },
-  ]);
-  const addTodo = (value) => {
-    const newTodos = [...todos, { text: value, isCompleted: false }];
-    setTodos(newTodos);
-  };
-
-  const removeTodo = (index) => {
-    let temp = [...todos];
-    temp.splice(index, 1);
-    setTodos(temp);
-  };
-
+function TodoList(props) {
   return (
-    <div className="app">
-      <h1>TODO</h1>
-      {/* <label>Add TODO</label> */}
-      <div className="todo-list">
-        {todos.map((todo, i) => (
-          <Todo key={i} index={i} todo={todo} remove={removeTodo} />
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
-    </div>
+    <ul>
+      {props.items.map((item, i) => (
+        <li key={i}>{item}</li>
+      ))}
+    </ul>
   );
 }
 
