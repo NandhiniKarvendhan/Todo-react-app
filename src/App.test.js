4c7e244d348193cb0b38b8fe06fcd59bc7b43c6a
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-12-05 11:32:33 +0530

    Rendering the  component

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,8 @@
-// import { render, screen } from '@testing-library/react';
-
-import { render } from "@testing-library/react";
 import React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
-// function render(component) {
-//   const root = document.createElement("div");
-//   ReactDOM.render(component, root);
-//   return getQueriesForElement(root);
-// }
-
 test("ToDo", () => {
-  const { getByText, getByLabelText } = render(<App />);
-
-  // after rendering our component
-  // use DOM APIs (query selector) to make assertions
-
-  // expect(root.querySelector("label").textContent).toBe("Add TODO");
-  expect(getByText("TODO")).not.toBeNull();
-  // getByLabelText("Add TODO");
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
 });
