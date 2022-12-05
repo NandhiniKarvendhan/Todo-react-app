706e065d7b31c8aed920795b467ebe92b7e81ac4
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-12-05 12:11:38 +0530

    Trimmer testing code

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,7 @@
-import { getQueriesForElement } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import React from "react";
-import * as ReactDOM from "react-dom";
 import App from "./App";
 
-//test the render functionality for your application
-function render(component) {
-  const root = document.createElement("div");
-  ReactDOM.render(component, root);
-  return getQueriesForElement(root);
-}
 test("ToDo", () => {
   const { getByText, getByLabelText } = render(<App />);
   // after rendering our component
