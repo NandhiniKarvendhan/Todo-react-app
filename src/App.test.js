23fb4bf60deafb77a0b8b5e21b68397bfa60f18d
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-12-05 11:59:59 +0530

    Testing using the React Library

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+import { getQueriesForElement } from "@testing-library/react";
 import React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
@@ -5,10 +6,11 @@ import App from "./App";
 test("ToDo", () => {
   const root = document.createElement("div");
   ReactDOM.render(<App />, root);
+  const { getByText, getByLabelText } = getQueriesForElement(root);
 
   // after rendering our component
-  // use DOM APIs (query selector) to make assertions
-  expect(root.querySelector("h1").textContent).toBe("TODO");
-  expect(root.querySelector("label").textContent).toBe("Add TODO:");
-  expect(root.querySelector("button").textContent).toBe("Add #1");
+  // Refactoring tests so they closely align with the user experience.
+  expect(getByText("TODO")).not.toBeNull();
+  expect(getByLabelText("Add TODO:")).not.toBeNull();
+  expect(getByText("Add #1")).not.toBeNull();
 });
