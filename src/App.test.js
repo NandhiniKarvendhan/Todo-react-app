0f743304898e68e264e92c0f8869ee87d909528e
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-12-06 10:58:56 +0530

    test input data with fireEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
-import { render } from "@testing-library/react";
 import React from "react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("ToDo", () => {
@@ -9,4 +9,19 @@ test("ToDo", () => {
   getByText("TODO");
   getByLabelText("Add TODO:");
   getByText("Add #1");
+});
+
+test("add items to list", () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  // after rendering our component
+  getByText("TODO");
+  const input = getByLabelText("Add TODO:");
+  fireEvent.change(input, { target: { value: "wash car" } });
+
+  fireEvent.click(getByText("Add #1"));
+
+  // confirm data
+  getByText("Add #2");
+  getByText("wash car");
 });
