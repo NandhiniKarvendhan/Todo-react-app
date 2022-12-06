9b34757386593699e8b505e8927d29734ce80cf5
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-12-06 11:08:40 +0530

    Testing User Event

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 test("ToDo", () => {
   const { getByText, getByLabelText } = render(<App />);
@@ -18,10 +19,22 @@ test("add items to list", () => {
   getByText("TODO");
   const input = getByLabelText("Add TODO:");
   fireEvent.change(input, { target: { value: "wash car" } });
-
   fireEvent.click(getByText("Add #1"));
 
   // confirm data
   getByText("Add #2");
   getByText("wash car");
+});
+
+// userEvent expresses intent better
+test("user-events allows users to add...", () => {
+  const { getByText, getByLabelText } = render(<App />);
+  const input = getByLabelText("Add TODO:");
+  const button = getByText("Add #1");
+
+  userEvent.type(input, "Learn spanish");
+  userEvent.click(button);
+
+  getByText("Learn spanish");
+  getByText("Add #2");
 });
